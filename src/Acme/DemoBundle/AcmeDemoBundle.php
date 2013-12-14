949b9fe8eaa28cc082b4f4a8fb7d8cdd4f9836bc
Romaric Drigon <romaric.drigon@gmail.com>
2013-12-14 12:24:39 +0100

    Added Wsse firewall

--- src/Acme/DemoBundle/AcmeDemoBundle.php
+++ src/Acme/DemoBundle/AcmeDemoBundle.php
@@ -3,7 +3,16 @@
 namespace Acme\DemoBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Acme\DemoBundle\DependencyInjection\Security\Factory\WsseFactory;
 
 class AcmeDemoBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new WsseFactory());
+    }
 }
